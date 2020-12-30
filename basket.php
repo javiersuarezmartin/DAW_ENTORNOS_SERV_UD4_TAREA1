30ef1c3d4a0bca40e5bf98023ad01ade70ba4ee6
Javier <jsuarezmartin1982@gmail.com>
2020-12-31 00:54:36 +0100

    Array Baloncesto

---
+++ basket.php
@@ -0,0 +1,27 @@
+<?php
+
+$equipos_basket = [
+    "Barça",
+    "BAXI Manresa",
+    "Casademont Zaragoza",
+    "Club Joventut Badalona",
+    "Coosur Real Betis",
+    "Acunsa GBC",
+    "Herbalife Gran Canaria",
+    "Iberostar Tenerife",
+    "Monbus Obradoiro",
+    "MoraBanc Andorra",
+    "Movistar Estudiantes",
+    "Real Madrid",
+    "RETAbet Bilbao Basket",
+    "Hereda San Pablo Burgos",
+    "TD Systems Baskonia",
+    "UCAM Murcia CB",
+    "Unicaja Málaga",
+    "Urbas Fuenlabrada",
+    "Valencia Basket Club"
+];
+
+print_r ($equipos_basket);
+
+?>
